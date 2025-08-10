d47c663415788509e101792222833a897ccd4a65
jeremiahcanlas <jeremiahcanlas@gmail.com>
2025-08-10 10:53:12 -0400

    Add location pin svg

---
+++ src/assets/vectors/index.tsx
@@ -0,0 +1,32 @@
+export const LocationPin = () => (
+  <svg
+    viewBox="-1.68 -1.68 27.36 27.36"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="0.144"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      {" "}
+      <path
+        d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+        stroke="#000000"
+        strokeWidth="1.8240000000000003"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>{" "}
+      <path
+        d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+        stroke="#000000"
+        strokeWidth="1.8240000000000003"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>{" "}
+    </g>
+  </svg>
+);
