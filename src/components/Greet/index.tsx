bd5dc9bf2e83fd74bb7a4edfc8659c545f5cdd3a
jeremiahcanlas <jeremiahcanlas@gmail.com>
2025-08-25 10:56:41 -0400

    [feat] Add usePersonalizedGreet custom hook

--- src/components/Greet/index.tsx
+++ src/components/Greet/index.tsx
@@ -1,15 +1,12 @@
-import { useDashboardSettings } from "../../context/DashboardSettingsContext";
+import { usePersonalizedGreet } from "../../hooks/greet";
 
 type GreetProp = {
   message: string;
 };
 
 const Greet = ({ message }: GreetProp): React.JSX.Element => {
-  const { username } = useDashboardSettings();
+  const { personalizedGreeting } = usePersonalizedGreet(message);
 
-  const personalizedGreeting = username
-    ? message.replace("{{name}}", username)
-    : message.replace(", {{name}}", "");
   return (
     <div className="uppercase max-w-[80vw]">
       <h1 className="whitespace-nowrap">{personalizedGreeting}</h1>
