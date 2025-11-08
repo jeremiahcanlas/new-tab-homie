6043d38aafbbe62eb455482bbed5f3683f71e08a
jeremiahcanlas <jeremiahcanlas@gmail.com>
2025-11-07 21:19:02 -0500

    Add handleAnimationEnd support in quote custom hook

--- src/hooks/quote/index.tsx
+++ src/hooks/quote/index.tsx
@@ -1,12 +1,17 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import type { QuoteData } from "../../types";
 import quotes from "../../data/quotes.json";
+import { useDashboardSettings } from "../../context/DashboardSettingsContext";
 
 export const useQuote = () => {
+  const { isQuoteToggled } = useDashboardSettings();
+
   const [quote, setQuote] = useState<QuoteData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const hasFetched = useRef(false);
+
+  const [shouldRender, setShouldRender] = useState(isQuoteToggled);
 
   const getRandomQuote = useCallback((): QuoteData => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -33,12 +38,25 @@ export const useQuote = () => {
     }
   }, [getRandomQuote]);
 
+  const handleAnimationEnd = () => {
+    if (!isQuoteToggled) setShouldRender(false);
+  };
+
   useEffect(() => {
-    if (!hasFetched.current) {
+    // Will only fetch if quote is toggled to avoid unecessary api calls
+    if (!hasFetched.current && isQuoteToggled) {
       hasFetched.current = true;
+      setShouldRender(true);
       fetchQuote();
     }
-  }, [fetchQuote]);
+  }, [fetchQuote, isQuoteToggled]);
 
-  return { quote, loading, error };
+  return {
+    quote,
+    loading,
+    error,
+    isQuoteToggled,
+    shouldRender,
+    handleAnimationEnd,
+  };
 };
